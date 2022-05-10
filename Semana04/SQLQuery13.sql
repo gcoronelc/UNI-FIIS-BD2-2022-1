d8c00979057f7d801134372ecaac69bf03f47488
Gustavo Coronel <gcoronelc@gmail.com>
2022-05-10 15:54:25 -0500

    Semana 4

--- Semana04/SQLQuery13.sql
+++ Semana04/SQLQuery13.sql
@@ -2,20 +2,24 @@ USE EDUCA;
 GO
 
 
-CREATE PROCEDURE dbo.usp_precio ( @p_idcurso int, @p_precio money OUT ) 
+ALTER PROCEDURE dbo.usp_precio ( @p_idcurso int, @p_estado int OUT, @p_precio money OUT ) 
 AS 
 BEGIN 
 	SELECT @p_precio = cur_precio 
 	FROM dbo.CURSO 
 	WHERE cur_id = @p_idcurso; 
+	SELECT @p_estado = @@ROWCOUNT;
 END;
 GO
 
 
 BEGIN 
-	DECLARE @precio money; 
-	EXEC dbo.usp_precio 7, @precio OUT; 
-	PRINT CONCAT( 'PRECIO: ', @precio ); 
+	DECLARE @estado int,  @precio money; 
+	EXEC dbo.usp_precio 20, @estado OUT, @precio OUT; 
+	IF (@estado=1)
+		PRINT CONCAT( 'PRECIO: ', @precio );
+	ELSE
+		PRINT 'ERROR: curso no existe.';
 END;
 GO
 
